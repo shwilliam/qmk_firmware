bbe6089214c84dface34f756a735f561a60ebf37
William Lindvall <shwilliam@hey.com>
2022-01-23 00:29:49 -0800

    update layer names

--- keyboards/crkbd/keymaps/shwilliam/keymap.c
+++ keyboards/crkbd/keymaps/shwilliam/keymap.c
@@ -87,19 +87,19 @@ void oled_render_layer_state(void) {
     oled_write_P(PSTR("Layer: "), false);
     switch (layer_state) {
         case L_BASE:
-            oled_write_ln_P(PSTR("Default"), false);
+            oled_write_ln_P(PSTR("BASE"), false);
             break;
         case L_LOWER:
-            oled_write_ln_P(PSTR("Lower"), false);
+            oled_write_ln_P(PSTR("SYMBOLS"), false);
             break;
         case L_RAISE:
-            oled_write_ln_P(PSTR("Raise"), false);
+            oled_write_ln_P(PSTR("NUMBERS"), false);
             break;
         case L_ADJUST:
         case L_ADJUST|L_LOWER:
         case L_ADJUST|L_RAISE:
         case L_ADJUST|L_LOWER|L_RAISE:
-            oled_write_ln_P(PSTR("Adjust"), false);
+            oled_write_ln_P(PSTR("SYSTEM"), false);
             break;
     }
 }
